136c22ca3bd3e354c8986ded0de56951b066faa3
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-03-18 03:05:21 +0700

    5 files modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,15 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './node_modules/flowbite/**/*.js',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  darkMode: 'light',
   theme: {
     extend: {},
     fontFamily: {
-      sans: [
-        'Inter',
-        'Avenir',
-        'Roboto',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Ubuntu',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ],
+      sans: ['Fredoka'],
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
